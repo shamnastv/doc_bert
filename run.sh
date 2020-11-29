6fbfe557961db19949b1a74b854748cb048f9620
Shamnas <shamnastv@gmail.com>
2020-11-29 13:09:59 +0530

    update

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH --job-name=nlp # Job name
 #SBATCH --ntasks=4 # Run on a single CPU
-#SBATCH --time=23:50:00 # Time limit hrs:min:sec
+#SBATCH --time=1:50:00 # Time limit hrs:min:sec
 #SBATCH --output=test_job%j.out # Standard output and error log
 #SBATCH --gres=gpu:1
-#SBATCH --partition=cl1_48h-1G
+#SBATCH --partition=cl1_2h-4G
 
 #printf "min freq 1\n"
 
